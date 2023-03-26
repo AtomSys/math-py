70f0a9b2b5a6ff833002a028653e605c5cc487bd
AtomSys <zcrenshaw1@icloud.com>
2023-03-26 16:33:12 -0400

    Added Addition, Subtraction, and Multiplication

---
+++ main.py
@@ -0,0 +1,7 @@
+def math(num1,num2,mode):
+    if mode == "a":
+        int(num1) + int(num2)
+    elif mode == "s":
+        int(num1) - int(num2)
+    elif mode == "m":
+        int(num1) * int(num2)
