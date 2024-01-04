71f72c4dd5dd9108a050753cc0397b37144db690
AtomSys <127058397+AtomSys@users.noreply.github.com>
2024-01-04 10:57:51 -0500

    Update main.py
    Added dedicated adding function ( add(n1, n2) )

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def math(num1,num2,mode):
     elif mode == "m":
         ans = int(num1) * int(num2)
         return ans
+def add(n1, n2):
+    ans = ""
+    ans = int(n1) + int(n2)
+    return ans
